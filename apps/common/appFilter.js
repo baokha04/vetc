e00d4f3f29d1aae45265b524f6ba4df5da64feaf
duyanh <duyanh2005@gmail.com>
2016-10-28 19:44:58 +0700

    add filter

---
+++ apps/common/appFilter.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+    angular.module('vetc.filter', [])
+        .factory('appFilter', ['$q', function ($q) {
+            var appFilter = function () {
+            }
+            
+            appFilter.prototype.listInstance = {                
+            }
+
+            appFilter.prototype.filter = function (array, keyword) {                
+                return keyword;
+            }
+            
+            return new appFilter;
+        }])
+})();
